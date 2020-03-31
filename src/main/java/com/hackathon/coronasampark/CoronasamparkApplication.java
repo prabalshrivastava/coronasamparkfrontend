646fabca392f9c07b98fff34d108d8d6de0baa9a
prabal shrivastava <prabalshrivastava54@gmail.com>
2020-03-31 21:17:16 +0530

    Added the spring boot empty project

---
+++ src/main/java/com/hackathon/coronasampark/CoronasamparkApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.coronasampark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronasamparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronasamparkApplication.class, args);
+    }
+
+}
